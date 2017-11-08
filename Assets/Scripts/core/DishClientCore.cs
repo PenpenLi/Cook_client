6931afa0fbeb9c51fb639f686440b4d43fe7c00c
ludi <ludidilu@163.com>
2017-11-08 16:56:15 +0800

    a

---
+++ Assets/Scripts/core/DishClientCore.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using Cook_lib;
+using System;
+
+public class DishClientCore : MonoBehaviour, IClient
+{
+    private Cook_client client;
+
+    void Awake()
+    {
+        client = new Cook_client();
+
+        client.Init(this);
+    }
+
+    public void RefreshData()
+    {
+
+    }
+
+    public void SendData(MemoryStream _ms)
+    {
+
+    }
+
+    public void SendData(MemoryStream _ms, Action<BinaryReader> _callBack)
+    {
+
+    }
+
+    public void TriggerEvent(ValueType _event)
+    {
+
+    }
+
+    public void UpdateCallBack()
+    {
+
+    }
+}
